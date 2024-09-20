cc18a7496d4255dc747d32dd7d6f70e9c4c4d5cc
Aimar Moreno <aimar.moreno@opendeusto.es>
2024-09-20 08:41:00 +0200

    sdgv<sdvzsdgasdf

--- PruebaGit/src/Prueba.java
+++ PruebaGit/src/Prueba.java
@@ -3,7 +3,7 @@
 public class Prueba {
 	public static void main(String[] args) {
 	 System.out.println("FOR PAIN AND FOR GLORY!!!");
-	 System.out.println("SOY EL MEJOOOR");
+	 System.out.println("SOY EL MEJOOORsdfzdgasdgahgeargadgfawetfaesgtfads");
 	 System.out.println("ME MUEROOO");
 	}
 }
