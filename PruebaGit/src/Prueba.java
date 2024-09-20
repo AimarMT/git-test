04341d0b4cf874ff46be2a3a8e52a31d4566438e
AimarMT <aimar.moreno@opendeusto.es>
2024-09-20 08:33:46 +0200

    Update Prueba.java

--- PruebaGit/src/Prueba.java
+++ PruebaGit/src/Prueba.java
@@ -4,5 +4,6 @@ public class Prueba {
 	public static void main(String[] args) {
 	 System.out.println("FOR PAIN AND FOR GLORY!!!");
 	 System.out.println("SOY EL MEJOOOR");
+	 System.out.println("ME MUEROOO");
 	}
 }
